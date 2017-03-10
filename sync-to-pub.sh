e727ac02fd85fd11f8abb164ba14c0d491e8ac50
rockeet <rockeet@gmail.com>
2017-03-10 13:34:36 +0800

    sync-to-pub.sh: minor fix

--- sync-to-pub.sh
+++ sync-to-pub.sh
@@ -1,5 +1,5 @@
 #/usr/bin/bash
 
 cp -r src ../terark-zip-rocksdb-pub
-sed -i '/TerocksPrivateCode/,/#endif.*TerocksPrivateCode/d' `find ../terark-zip-rocksdb-pub -type f`
+sed -i '/TerocksPrivateCode/,/#endif.*TerocksPrivateCode/d' `find ../terark-zip-rocksdb-pub/src -type f -a '(' -name '*.cc' -o -name '*.h' ')'`
 
