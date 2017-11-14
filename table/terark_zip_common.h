e28d14b81dc18d6f7f58e3c37d992d634087b2e3
rockeet <rockeet@gmail.com>
2017-11-14 21:11:01 +0800

    Fix for readability

--- table/terark_zip_common.h
+++ table/terark_zip_common.h
@@ -45,7 +45,19 @@ using terark::LittleEndianDataOutput;
 template<class T>
 inline unique_ptr<T> UniquePtrOf(T* p) { return unique_ptr<T>(p); }
 
+uint64_t ReadUint64(const byte_t* beg, size_t len);
 uint64_t ReadUint64(const byte_t* beg, const byte_t* end);
+
+inline
+uint64_t ReadUint64Aligned(const byte_t* beg, size_t len) {
+  assert(8 == len); (void)len;
+#if BOOST_ENDIAN_LITTLE_BYTE
+  return terark::byte_swap(*(const uint64_t*)beg);
+#else
+  return *(const uint64_t*)beg;
+#endif
+}
+
 uint64_t ReadUint64Aligned(const byte_t* beg, const byte_t* end);
 void AssignUint64(byte_t* beg, byte_t* end, uint64_t value);
 
