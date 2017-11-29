b7407a9453c8aff1cbd707081f1b776537158f1a
squalfof <squalfof@gmail.com>
2017-11-29 21:00:28 +0800

    Add rank_select_fewone

--- table/tests/rank_select_fewzero.h
+++ table/tests/rank_select_fewzero.h
@@ -135,7 +135,8 @@ private:
     size_t i = low, j = upp;
     while (i < j) {
       size_t mid = (i + j) / 2;
-      size_t t = rank1_from_rank0(mid);
+      //size_t t = rank1_from_rank0(mid);
+      size_t t = m_pospool[mid] - mid + 1;
       if (t < rank1)
         i = mid + 1;
       else
@@ -149,7 +150,8 @@ private:
    */
   bool is1(size_t pos, size_t& res) const {
     assert(pos < m_size);
-    res = terark::lower_bound_n(m_pospool, 1, m_pospool.size(), pos);
+    res = terark::lower_bound_n<const terark::valvec<size_t>&>(
+      m_pospool, 1, m_pospool.size(), pos);
     if (res >= m_pospool.size()) { // not in '0's
       return true;
     } else {
@@ -227,7 +229,7 @@ public:
     size_t i;
     if (!is1(pos, i))
       return 0;
-    if (i >= m_pospool.size() - 1)
+    if (i >= m_pospool.size())
       return m_size - pos;
     else
       return m_pospool[i] - pos;
@@ -250,3 +252,224 @@ private:
   size_t m_size;
   size_t m_prev;
 };
+
+
+// make sure at least one '1' and at least one '0'
+class rank_select_fewone {
+public:
+  typedef boost::mpl::false_ is_mixed;
+  rank_select_fewone() : m_size(0), m_prev(-1) {}
+  explicit rank_select_fewone(size_t sz) :
+    m_size(sz),
+    m_prev(-1) {}
+    
+  void clear() { 
+    m_size = 0;
+    m_prev = size_t(-1);
+    m_pospool.clear();
+  }
+  void risk_release_ownership() {
+    m_pospool.risk_release_ownership();
+  }
+  void risk_mmap_from(unsigned char* base, size_t length) {
+    // TBD: ? m_size = *((size_t*)base);
+    m_pospool.risk_set_data((size_t*)base, length);
+  }
+
+  void resize(size_t newsize) {
+    m_size = newsize;
+    m_pospool.resize(newsize);
+  }
+  void swap(rank_select_fewone& another) {
+    std::swap(m_size, another.m_size);
+    std::swap(m_pospool, another.m_pospool);
+  }
+
+  template<class RankSelect>
+  void build_from(RankSelect& rs) {
+    // TBD: for fewzero, 0.01 is actually reasonable
+    //m_pospool.resize(m_size / 10);
+    m_pospool.resize(m_size + 1);
+    m_pospool[0] = 0;
+    size_t idx = 1;
+    for (size_t pos = 0; pos < m_size; pos++) {
+      if (rs.is1(pos)) {
+        m_pospool[idx] = pos;
+        idx ++;
+      }
+    }
+    m_pospool.resize(idx);
+  }
+  size_t mem_size() const { return sizeof(m_size) + m_pospool.used_mem_size(); }
+
+  // exclude pos
+  size_t rank0(size_t pos) const {
+    assert(pos < m_size);
+    return pos - rank1(pos);
+  }
+
+  size_t rank1(size_t pos) const {
+    assert(pos < m_size);
+    RET_IF_ZERO(pos);
+    pos --;
+    size_t rank = terark::lower_bound_n<const valvec<size_t>&>(
+      m_pospool, 1, m_pospool.size(), pos);
+    if (rank == m_pospool.size()) {
+      return m_pospool.size() - 1;
+    } else if (pos == m_pospool[rank]) {
+      return rank;
+    } else { // < 
+      return rank - 1;
+    }
+  }
+
+  size_t select0(size_t id) const {
+    RET_IF_ZERO(id);
+    size_t i = lower_bound(1, m_pospool.size(), id);
+    // TBD: possible consecutive m_pos[i] + 1 == m_pos[i + 1]
+    // make sure arr[m_pospool[i] - 1] is '1' not '0'
+    if (i < m_pospool.size() && id == rank0_from_rank1(i))
+      return m_pospool[i] - 1;
+    if (i >= m_pospool.size()) { // beyond the last '0'
+      i = m_pospool.size() - 1;
+    } else { // move to prev '0'
+      i --; 
+    }
+    size_t prev = rank0_from_rank1(i);
+    size_t offset = id - prev;
+    return m_pospool[i] + offset;
+  }
+  size_t select1(size_t id) const {
+    assert(id < m_size);
+    if (id >= m_pospool.size())
+      return size_t(-1);
+    return m_pospool[id];
+  }
+
+private:
+  size_t rank0_from_rank1(size_t i) const {
+    assert(i < m_pospool.size());
+    return m_pospool[i] - i + 1;
+  }
+  // return rank1 that m_pospool[rank1] >= rank0's position
+  size_t lower_bound(size_t low, size_t upp, const size_t& rank0) const {
+    size_t i = low, j = upp;
+    while (i < j) {
+      size_t mid = (i + j) / 2;
+      //size_t t = rank0_from_rank1(mid);
+      size_t t = m_pospool[mid] - mid + 1;
+      if (t < rank0)
+        i = mid + 1;
+      else
+        j = mid;
+    }
+    return i;
+  }
+  
+  // res: as the rank0 lower_bound
+  bool is0(size_t pos, size_t& res) const {
+    assert(pos < m_size);
+    res = terark::lower_bound_n<const terark::valvec<size_t>&>(
+      m_pospool, 1, m_pospool.size(), pos);
+    if (res >= m_pospool.size()) { // not in '1's
+      return true;
+    } else {
+      return (pos != m_pospool[res]);
+    }
+  }
+
+public:
+  size_t max_rank0() const { return m_size - (m_pospool.size() - 1); }
+  size_t max_rank1() const { return m_pospool.size() - 1; }
+  size_t size() const { return m_size; }
+
+  const void* data() const { return m_pospool.data(); }
+  bool operator[](size_t pos) const {
+    assert(pos < m_size);
+    return is1(pos);
+  }
+  // TBD: use hash_set maybe better
+  bool is0(size_t pos) const {
+    assert(pos < m_size);
+    size_t res;
+    return is0(pos, res);
+  }
+  bool is1(size_t pos) const {
+    assert(pos < m_size);
+    return !is0(pos);
+  }
+
+  const uint32_t* get_rank_cache() const { return NULL; }
+  const uint32_t* get_sel0_cache() const { return NULL; }
+  const uint32_t* get_sel1_cache() const { return NULL; }
+
+  size_t zero_seq_len(size_t pos) const {
+    assert(pos < m_size);
+    size_t i;
+    if (!is0(pos, i))
+      return 0;
+    if (i >= m_pospool.size())
+      return m_size - pos;
+    else
+      return m_pospool[i] - pos;
+  }
+  size_t zero_seq_revlen(size_t pos) const {
+    assert(pos < m_size);
+    RET_IF_ZERO(pos);
+    pos --;
+    size_t i;
+    if (!is0(pos, i))
+      return 0;
+    if (i == 1)
+      return pos + 1;
+    else
+      return pos - m_pospool[i - 1];
+  }
+
+  ///@returns number of continuous one/zero bits starts at bitpos
+  // TBD: has one prev state stored, which may helpful to avoid
+  //  invoking is1() during Next()/Prev()?
+  size_t one_seq_len(size_t pos) const {
+    assert(pos < m_size);
+    // to accelerate Next()
+    //if (0 < m_prev && m_prev < m_pospool.size() + 1 &&
+    //    m_pospool[m_prev] < pos && pos < m_pospool[m_prev + 1]) {
+    //  return 0;
+    //}
+    size_t i;
+    if (is0(pos, i)) {
+      //m_prev = i - 1;
+      return 0;
+    }
+    size_t cnt = 1;
+    while (i < m_pospool.size() - 1) {
+      if (m_pospool[i] + 1 == m_pospool[i + 1])
+        i++, cnt++;
+      else
+        break;
+    }
+    return cnt;
+  }
+  size_t one_seq_revlen(size_t pos) const {
+    assert(pos < m_size);
+    RET_IF_ZERO(pos);
+    pos --;
+    size_t i;
+    if (is0(pos, i))
+      return 0;
+    size_t cnt = 1;
+    while (i > 1) { // m_pospool[0] is placeholder
+      if (m_pospool[i - 1] + 1 == m_pospool[i])
+        i--, cnt++;
+      else
+        break;
+    }
+    return cnt;
+  }
+
+private:
+  valvec<size_t> m_pospool;
+  size_t m_size;
+  size_t m_prev;
+};
+
--- table/tests/rs_fewzero_ut.cc
+++ table/tests/rs_fewzero_ut.cc
@@ -36,8 +36,8 @@ using rocksdb::TerarkIndex;
  * pos: 0 1 2 3 4 5 6
  *      1 0 1 1 0 0 1
  */
-void test_simple() {
-  printf("==== Test started\n");
+void test_fewzero() {
+  printf("==== Test fewzero\n");
   const size_t kCnt = 7;
   terark::rank_select_simple simple(kCnt);
   size_t pos = 0;
@@ -142,9 +142,121 @@ void test_simple() {
   printf("\tdone\n");
 }
 
+/*
+ * pos: 0 1 2 3 4 5 6
+ *      1 0 1 1 0 0 1
+ */
+void test_fewone() {
+  printf("==== Test fewone\n");
+  const size_t kCnt = 7;
+  terark::rank_select_simple simple(kCnt);
+  size_t pos = 0;
+  simple.set1(pos++); simple.set0(pos++);
+  simple.set1(pos++); simple.set1(pos++);
+  simple.set0(pos++); simple.set0(pos++);
+  simple.set1(pos++);
+  simple.build_cache(true, true);
+  
+  rank_select_fewone rs(kCnt);
+  rs.build_from(simple);
+  {
+    assert(rs.zero_seq_revlen(0) == 0);
+    assert(rs.zero_seq_revlen(1) == 0);
+    assert(rs.zero_seq_revlen(2) == 1);
+    assert(rs.zero_seq_revlen(3) == 0);
+    assert(rs.zero_seq_revlen(4) == 0);
+    assert(rs.zero_seq_revlen(5) == 1);
+    assert(rs.zero_seq_revlen(6) == 2);
+  }
+  {
+    assert(rs.one_seq_revlen(0) == 0);
+    assert(rs.one_seq_revlen(1) == 1);
+    assert(rs.one_seq_revlen(2) == 0);
+    assert(rs.one_seq_revlen(3) == 1);
+    assert(rs.one_seq_revlen(4) == 2);
+    assert(rs.one_seq_revlen(5) == 0);
+    assert(rs.one_seq_revlen(6) == 0);
+  }
+  {
+    assert(rs.zero_seq_len(0) == 0);
+    assert(rs.zero_seq_len(1) == 1);
+    assert(rs.zero_seq_len(2) == 0);
+    assert(rs.zero_seq_len(3) == 0);
+    assert(rs.zero_seq_len(4) == 2);
+    assert(rs.zero_seq_len(5) == 1);
+    assert(rs.zero_seq_len(6) == 0);
+  }
+  {
+    assert(rs.one_seq_len(0) == 1);
+    assert(rs.one_seq_len(1) == 0);
+    assert(rs.one_seq_len(2) == 2);
+    assert(rs.one_seq_len(3) == 1);
+    assert(rs.one_seq_len(4) == 0);
+    assert(rs.one_seq_len(5) == 0);
+    assert(rs.one_seq_len(6) == 1);
+  }
+  {
+    assert(rs.is0(0) == false);
+    assert(rs.is0(1) == true);
+    assert(rs.is0(2) == false);
+    assert(rs.is0(3) == false);
+    assert(rs.is0(4) == true);
+    assert(rs.is0(5) == true);
+    assert(rs.is0(6) == false);
+  }
+  {
+    assert(rs.is1(0) == true);
+    assert(rs.is1(1) == false);
+    assert(rs.is1(2) == true);
+    assert(rs.is1(3) == true);
+    assert(rs.is1(4) == false);
+    assert(rs.is1(5) == false);
+    assert(rs.is1(6) == true);
+  }
+  {
+    assert(rs.rank0(0) == 0);
+    assert(rs.rank0(1) == 0);
+    assert(rs.rank0(2) == 1);
+    assert(rs.rank0(3) == 1);
+    assert(rs.rank0(4) == 1);
+    assert(rs.rank0(5) == 2);
+    assert(rs.rank0(6) == 3);
+  }
+  {
+    assert(rs.rank1(0) == 0);
+    assert(rs.rank1(1) == 1);
+    assert(rs.rank1(2) == 1);
+    assert(rs.rank1(3) == 2);
+    assert(rs.rank1(4) == 3);
+    assert(rs.rank1(5) == 3);
+    assert(rs.rank1(6) == 3);
+  }
+  {
+    assert(rs.select0(0) == 0);
+    assert(rs.select0(1) == 1);
+    assert(rs.select0(2) == 4);
+    assert(rs.select0(3) == 5);
+  }
+  {
+    assert(rs.select1(0) == 0);
+    assert(rs.select1(1) == 0);
+    assert(rs.select1(2) == 2);
+    assert(rs.select1(3) == 3);
+    assert(rs.select1(4) == 6);
+  }
+  {
+    assert(rs.max_rank0() == 3);
+    assert(rs.max_rank1() == 4);
+  }
+
+  printf("\tdone\n");
+}
+
+
 int main() {
   printf("EXAGGERATE\n");
-  test_simple();
+  test_fewzero();
+  test_fewone();
 
   return 0;
 }
