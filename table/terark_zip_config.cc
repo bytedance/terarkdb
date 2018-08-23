217ec8c2590cd7b2fc71ea3dc56832b235ecd7c3
rockeet <rockeet@gmail.com>
2018-08-23 14:28:31 +0800

    change some default env conf

--- table/terark_zip_config.cc
+++ table/terark_zip_config.cc
@@ -237,7 +237,7 @@ bool TerarkZipCFOptionsFromEnv(ColumnFamilyOptions& cfo) {
   MyGetInt   (tzo, terarkZipMinLevel       , 0    );
   MyGetInt   (tzo, debugLevel              , 0    );
   MyGetInt   (tzo, keyPrefixLen            , 0    );
-  MyGetInt   (tzo, offsetArrayBlockUnits   , 0    );
+  MyGetInt   (tzo, offsetArrayBlockUnits   , 128  );
   MyGetInt   (tzo, indexNestScale          , 8    );
   if (true
       &&   0 != tzo.offsetArrayBlockUnits
@@ -265,7 +265,7 @@ bool TerarkZipCFOptionsFromEnv(ColumnFamilyOptions& cfo) {
   MyGetBool  (tzo, enableCompressionProbe  , true );
 
   MyGetDouble(tzo, estimateCompressionRatio, 0.20 );
-  MyGetDouble(tzo, sampleRatio             , 0.03 );
+  MyGetDouble(tzo, sampleRatio             , 0.06 );
   MyGetDouble(tzo, indexCacheRatio         , 0.00 );
 
   MyGetInt(tzo, minPreadLen, -1);
@@ -275,7 +275,7 @@ bool TerarkZipCFOptionsFromEnv(ColumnFamilyOptions& cfo) {
   MyGetXiB(tzo, smallTaskMemory);
   MyGetXiB(tzo, singleIndexMemLimit);
   MyGetXiB(tzo, cacheCapacityBytes);
-  MyGetInt(tzo, cacheShards, 17);
+  MyGetInt(tzo, cacheShards, 67);
   MyGetInt(tzo, minDictZipValueSize, 15);
 
   tzo.singleIndexMemLimit = std::min<size_t>(tzo.singleIndexMemLimit, 0x1E0000000);
