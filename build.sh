f13ff5dd54f4c459dd948588a10aad0c19f6d2b7
guokuankuan <guokuankuan@bytedance.com>
2019-04-28 10:39:22 +0800

    add build.sh

--- build.sh
+++ build.sh
@@ -1,10 +1,11 @@
 #!/usr/bin/env bash
 
-make libzstd.a libsnappy.a liblz4.a libbz2.a -j 4
-make shared_lib DEBUG_LEVEL=0 -j 4 DISABLE_WARNING_AS_ERROR=1
+#make libzstd.a libsnappy.a liblz4.a libbz2.a -j 4
+#make shared_lib DEBUG_LEVEL=0 -j 4 DISABLE_WARNING_AS_ERROR=1
 
 
 pkgdir=pkg
+# copy all header files
 mkdir -p $pkgdir
 cp -r db        $pkgdir
 cp -r env       $pkgdir
@@ -15,15 +16,19 @@ cp -r table     $pkgdir
 cp -r util      $pkgdir
 
 
-
-# move all binaries to output/ dir for next CICD steps
-WITH_BMI2=`./cpu_has_bmi2.sh`
+# detect output dir name
+WITH_BMI2=0
 SYSTEM=`uname -m -s | sed 's:[ /]:-:g'`
 tmpfile=`mktemp compiler-XXXXXX`
-COMPILER=`gcc tools/configure/compiler.cpp -o $tmpfile.exe && ./$tmpfile.exe && rm -f $tmpfile*`
+COMPILER=`gcc terark-tools/detect-compiler.cpp -o $tmpfile.exe && ./$tmpfile.exe && rm -f $tmpfile*`
 PLATFORM_DIR=$SYSTEM-$COMPILER-bmi2-$WITH_BMI2
 
-echo $PLATFORM_DIR
+echo build/$PLATFORM_DIR/shared_lib/dbg-0/
 
-#mv 'pkg/terark-fsa_all-'$PLATFORM_DIR output
+# copy dynamic lib
+mkdir -p $pkgdir/lib
+cp build/$PLATFORM_DIR/shared_lib/dbg-0/librocksdb.* $pkgdir/lib
+
+# change directory to fit CICD directory
+mv pkg output
 
