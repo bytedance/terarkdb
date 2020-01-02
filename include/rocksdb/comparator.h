bd0d31fa7f2208505491bbc298a260310a8f56b4
leipeng <leipeng.tk@bytedance.com>
2020-01-02 14:50:32 +0800

    try fix gcc compile error

--- include/rocksdb/comparator.h
+++ include/rocksdb/comparator.h
@@ -129,9 +129,14 @@ StdComparareEqualType<Comp> StdCompareEqual(const Comp* cmp) {
   return StdComparareEqualType<Comp>{cmp};
 }
 
+// gcc fails with
+//   sorry, unimplemented: string literal in function template signature
+// if we write cmp.Compare("",""), so use SFINAE_STR
+static const char SFINAE_STR[1] = "";
+
 template<class KeyExtractor, class KeyComparator>
 auto operator<(const KeyExtractor& ex, const KeyComparator& cmp) ->
-decltype(cmp.Compare("",""),
+decltype(cmp.Compare(SFINAE_STR,SFINAE_STR),
          terark::ExtractorComparator(ex, StdCompareLess(&cmp)))
 {
   return terark::ExtractorComparator(ex, StdCompareLess(&cmp));
@@ -139,7 +144,7 @@ decltype(cmp.Compare("",""),
 
 template<class KeyExtractor, class KeyComparator>
 auto operator>(const KeyExtractor& ex, const KeyComparator& cmp) ->
-decltype(cmp.Compare("",""),
+decltype(cmp.Compare(SFINAE_STR,SFINAE_STR),
          terark::ExtractorComparator(ex, StdCompareGreater(&cmp)))
 {
   return terark::ExtractorComparator(ex, StdCompareGreater(&cmp));
@@ -147,7 +152,7 @@ decltype(cmp.Compare("",""),
 
 template<class KeyExtractor, class KeyComparator>
 auto operator==(const KeyExtractor& ex, const KeyComparator& cmp) ->
-decltype(cmp.Compare("",""),
+decltype(cmp.Compare(SFINAE_STR,SFINAE_STR),
          terark::ExtractorComparator(ex, StdCompareEqual(&cmp)))
 {
   return terark::ExtractorComparator(ex, StdCompareEqual(&cmp));
@@ -155,19 +160,19 @@ decltype(cmp.Compare("",""),
 
 template<class KeyComparator>
 auto operator<(const char[1], const KeyComparator& cmp) ->
-decltype(cmp.Compare("",""), StdCompareLess(&cmp))
+decltype(cmp.Compare(SFINAE_STR,SFINAE_STR), StdCompareLess(&cmp))
 {
   return StdCompareLess(&cmp);
 }
 template<class KeyComparator>
 auto operator>(const char[1], const KeyComparator& cmp) ->
-decltype(cmp.Compare("",""), StdCompareGreater(&cmp))
+decltype(cmp.Compare(SFINAE_STR,SFINAE_STR), StdCompareGreater(&cmp))
 {
   return StdCompareGreater(&cmp);
 }
 template<class KeyComparator>
 auto operator==(const char[1], const KeyComparator& cmp) ->
-decltype(cmp.Compare("",""), StdCompareEqual(&cmp))
+decltype(cmp.Compare(SFINAE_STR,SFINAE_STR), StdCompareEqual(&cmp))
 {
   return StdCompareEqual(&cmp);
 }
@@ -199,19 +204,19 @@ struct Cmp3_to_Equal {
 
 template<class Cmp3>
 auto operator<(const char[1], const Cmp3& cmp) ->
-decltype(cmp("",""), Cmp3_to_Less<Cmp3>{&cmp})
+decltype(cmp(SFINAE_STR,SFINAE_STR), Cmp3_to_Less<Cmp3>{&cmp})
 {
   return Cmp3_to_Less<Cmp3>{&cmp};
 }
 template<class Cmp3>
 auto operator>(const char[1], const Cmp3& cmp) ->
-decltype(cmp("",""), Cmp3_to_Greater<Cmp3>{&cmp})
+decltype(cmp(SFINAE_STR,SFINAE_STR), Cmp3_to_Greater<Cmp3>{&cmp})
 {
   return Cmp3_to_Greater<Cmp3>{&cmp};
 }
 template<class Cmp3>
 auto operator>(const char[1], const Cmp3& cmp) ->
-decltype(cmp("",""), Cmp3_to_Equal<Cmp3>{&cmp})
+decltype(cmp(SFINAE_STR,SFINAE_STR), Cmp3_to_Equal<Cmp3>{&cmp})
 {
   return Cmp3_to_Equal<Cmp3>{&cmp};
 }
