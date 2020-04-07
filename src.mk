e022b6d5e66688a296bc4b228b84a03601125d3d
Sagar Vemuri <svemuri@fb.com>
2020-04-07 11:53:00 -0700

    [feature] cherry pick rocksdb's timer #5

--- src.mk
+++ src.mk
@@ -406,6 +406,7 @@ MAIN_SOURCES =                                                          \
   util/repeatable_thread_test.cc                                        \
   util/slice_transform_test.cc                                          \
   util/timer_queue_test.cc                                              \
+  util/timer_test.cc                                                    \
   util/thread_list_test.cc                                              \
   util/thread_local_test.cc                                             \
   utilities/backupable/backupable_db_test.cc                            \
