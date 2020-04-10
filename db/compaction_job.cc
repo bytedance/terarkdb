442b6296970badb220ccd99fe469677517db4d4b
leipeng <leipeng.tk@bytedance.com>
2020-04-10 18:36:13 +0800

    TablePropertiesCollectorFactory::Serialize(): Add param Context - related fix

--- db/compaction_job.cc
+++ db/compaction_job.cc
@@ -672,6 +672,8 @@ Status CompactionJob::Run() {
   context.compaction_filter_context.is_manual_compaction =
       c->is_manual_compaction();
   context.compaction_filter_context.column_family_id = cfd->GetID();
+  context.compaction_filter_context.smallest_user_key = compact_->SmallestUserKey();
+  context.compaction_filter_context.smallest_user_key = compact_->LargestUserKey();
   if (auto cf = iopt->compaction_filter) {
     s = cf->Serialize(&context.compaction_filter_data.data);
     if (s.IsNotSupported()) {
@@ -733,10 +735,14 @@ Status CompactionJob::Run() {
   context.bottommost_level = c->bottommost_level();
   context.allow_ingest_behind = iopt->allow_ingest_behind;
   context.preserve_deletes = iopt->preserve_deletes;
+  TablePropertiesCollectorFactory::Context collector_context;
+  collector_context.column_family_id = context.compaction_filter_context.column_family_id;
+  collector_context.smallest_user_key = context.smallest_user_key;
+  collector_context.largest_user_key = context.largest_user_key;
   for (auto& collector : *cfd->int_tbl_prop_collector_factories()) {
     std::string param;
     if (collector->NeedSerialize()) {
-      collector->Serialize(&param);
+      collector->Serialize(&param, collector_context);
     }
     context.int_tbl_prop_collector_factories.push_back(
         {collector->Name(), {std::move(param)}});
