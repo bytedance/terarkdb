d0dedb8e4e1db4dbb8ffac6a1c220305be8b7b55
liuyangming <liuyangming@bytedance.com>
2020-05-13 13:26:48 +0800

    deliver disableCompressDict to tiopt.compressGlobalDict

--- table/terark_zip_table_builder.cc
+++ table/terark_zip_table_builder.cc
@@ -822,6 +822,7 @@ void TerarkZipTableBuilder::BuildIndex(KeyValueStatus& kvs, size_t entropyLen) {
           tiopt.indexType = table_options_.indexType;
           tiopt.localTempDir = table_options_.localTempDir;
           tiopt.smallTaskMemory = table_options_.smallTaskMemory;
+          tiopt.compressGlobalDict = table_options_.disableCompressDict? false : true;
           auto& ref = tiopt;
           indexPtr.reset(TerarkIndex::Factory::Build(tempKeyFileReader.get(),
                                                      ref, keyStat, nullptr));
