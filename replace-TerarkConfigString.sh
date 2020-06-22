3e816a2f75f5a4bf14eed33f2cce8945a31b25b7
leipeng <leipeng.tk@bytedance.com>
2020-06-22 12:38:47 +0800

    change replace-TerarkConfigString.sh

--- replace-TerarkConfigString.sh
+++ replace-TerarkConfigString.sh
@@ -3,6 +3,5 @@
 #
 #   sh replace-TerarkConfigString.sh sample-TerarkEnv.sh mysql.cnf
 #
-< "$1" sed 's/export[ \t][\t]*//' | tr '\r\n' ';' | awk 'BEGIN{getline x < "/dev/stdin"}/%TerarkConfigString%/{gsub("%TerarkConfigString%",x)}1' "$2" > "/tmp/$2.tmp"
-mv "/tmp/$2.tmp" "$2"
+< "$1" sed 's/export[ \t][\t]*//' | tr '\r\n' ';' | awk 'BEGIN{getline x < "/dev/stdin"}/%TerarkConfigString%/{gsub("%TerarkConfigString%",x)}1' "$2"
 
