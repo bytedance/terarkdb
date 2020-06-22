8f2a49c9aea514f82241678edff152507dd19e70
leipeng <leipeng.tk@bytedance.com>
2020-06-22 12:07:48 +0800

    Add replace-TerarkConfigString.sh

---
+++ replace-TerarkConfigString.sh
@@ -0,0 +1,4 @@
+
+< "$1" sed 's/export[ \t][\t]*//' | tr '\r\n' ';' | awk 'BEGIN{getline x < "/dev/stdin"}/%TerarkConfigString%/{gsub("%TerarkConfigString%",x)}1' "$2" > "/tmp/$2.tmp"
+mv "/tmp/$2.tmp" "$2"
+
