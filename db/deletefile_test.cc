fbb6fc5f546659e653e3dbcdb4c0e2f7f99b2f9e
chenchanglong <chenchanglong@bytedance.com>
2020-07-31 16:12:25 +0800

    fix deletefile_test

--- db/deletefile_test.cc
+++ db/deletefile_test.cc
@@ -285,6 +285,11 @@ TEST_F(DeleteFileTest, BackgroundPurgeCFDropTest) {
     WriteOptions wo;
     FlushOptions fo;
     ColumnFamilyHandle* cfh = nullptr;
+    
+    rocksdb::SyncPoint::GetInstance()->LoadDependency({
+      {"DeleteFileTest::BackgroundPurgeCFDropTest:1",
+       "DeleteScheduler::BackgroundEmptyTrash"},
+    });
 
     ASSERT_OK(db_->CreateColumnFamily(co, "dropme", &cfh));
 
@@ -298,22 +303,12 @@ TEST_F(DeleteFileTest, BackgroundPurgeCFDropTest) {
     CheckFileTypeCounts(dbname_, 0, 1, 1);
 
     delete cfh;
-    test::SleepingBackgroundTask sleeping_task_after;
-    for (int i = 0, e = env_->GetBackgroundThreads(Env::Priority::HIGH); i < e; ++i) {
-      env_->Schedule(&test::SleepingBackgroundTask::DoSleepTask,
-                   &sleeping_task_after, Env::Priority::HIGH);
-    }
-    for (int i = 0, e = env_->GetBackgroundThreads(Env::Priority::LOW); i < e; ++i) {
-      env_->Schedule(&test::SleepingBackgroundTask::DoSleepTask,
-                   &sleeping_task_after, Env::Priority::LOW);
-    }
     
     // If background purge is enabled, the file should still be there.
     CheckFileTypeCounts(dbname_, 0, bg_purge ? 1 : 0, 1);
-
+    TEST_SYNC_POINT("DeleteFileTest::BackgroundPurgeCFDropTest:1");
     // Execute background purges.
-    sleeping_task_after.WakeUp();
-    sleeping_task_after.WaitUntilDone();
+
     // The file should have been deleted.
     CheckFileTypeCounts(dbname_, 0, 0, 1);
   };
