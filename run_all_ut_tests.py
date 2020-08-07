60e624f4e39ba66ca7a9118736e2e9e87aa37b16
chenchanglong <chenchanglong@bytedance.com>
2020-08-07 18:59:09 +0800

    modify cicd script

--- run_all_ut_tests.py
+++ run_all_ut_tests.py
@@ -4,7 +4,13 @@ from os import walk
 import subprocess
 import sys
 
-SKIP_LIST = ['db_bloom_filter_test', 'transaction_test', 'fault_injection_test', 'perf_context_test', 'obsolete_files_test']
+SKIP_LIST = ['db_bloom_filter_test',
+             'transaction_test',
+             'fault_injection_test',
+             'obsolete_files_test',
+             'db_test',
+             'backupable_db_test',
+             'db_merge_operator_test']
 TEST_LIST = []
 
 def run_test(name):
@@ -23,8 +29,28 @@ if __name__ == '__main__':
         break
 
     # run all tests
-    for testname in TEST_LIST:
-        code = run_test(testname)
-        if code != 0:
-            print 'unit test failed: %s, code = %d' % (testname, code)
-            sys.exit(code)
+    if sys.argv[1] != None:
+        passed = []
+        failed = []
+        for testname in TEST_LIST:
+            code = run_test(testname)
+            if code != 0:
+                failed.insert(testname)
+            else:
+                passed.insert(testname)
+        passed.sort()
+        failed.sort()
+        for e in passed:
+            e = '[x] ' + e
+        for e in failed:
+            e = '[ ] ' + e
+        f = open(sys.argv[1], "a+w")
+        f.writelines(failed)
+        f.writelines(passed) 
+        f.close()
+    else:
+        for testname in TEST_LIST:
+            code = run_test(testname)
+            if code != 0:
+                print 'unit test failed: %s, code = %d' % (testname, code)
+                sys.exit(code)
