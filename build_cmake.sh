1615df584996b8836ba8a61063652841c981f92b
guokuankuan <guokuankuan@bytedance.com>
2020-09-18 16:50:18 +0800

    fix cmake

--- build_cmake.sh
+++ build_cmake.sh
@@ -3,5 +3,17 @@
 BASE=$PWD
 OUTPUT=output
 mkdir -p $OUTPUT
+
+if test -n "$BUILD_BRANCH"; then
+    # this script is run in SCM auto build
+    git checkout "$BUILD_BRANCH"
+    sudo apt-get update
+    sudo apt-get install libaio-dev
+else
+    echo you must ensure libaio-dev have been installed
+fi
+
+git submodule --init --recursive
+
 cd $BASE/$OUTPUT && cmake ../ -DCMAKE_BUILD_TYPE=Release -DWITH_TESTS=OFF
 cd $BASE/$OUTPUT && make -j $(nproc) && make install
