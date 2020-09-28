e5ca903854ca493ad4d9156f4b05646c9838b854
guokuankuan <guokuankuan@bytedance.com>
2020-09-28 17:18:41 +0800

    refine scripts

--- build_cmake.sh
+++ build_cmake.sh
@@ -1,8 +1,21 @@
 #!/bin/bash
+#
+# WITH_TESTS=1 ./build_cmake.sh
+#
 
 BASE=$PWD
 OUTPUT=output
 mkdir -p $OUTPUT
+
+BUILD_TYPE=Release
+if [ "$WITH_TESTS" == "1" ];then
+  BUILD_TYPE=Debug
+  WITH_TESTS=ON
+else
+  WITH_TESTS=OFF
+fi
+
+echo "build $BUILD_TYPE, with_tests = $WITH_TESTS"
 
 if test -n "$BUILD_BRANCH"; then
     # this script is run in SCM auto build
@@ -15,5 +28,5 @@ fi
 
 git submodule update --init --recursive
 
-cd $BASE/$OUTPUT && cmake ../ -DCMAKE_BUILD_TYPE=Release -DWITH_TESTS=OFF
+cd $BASE/$OUTPUT && cmake ../ -DCMAKE_BUILD_TYPE=$BUILD_TYPE -DWITH_TESTS=$WITH_TESTS
 cd $BASE/$OUTPUT && make -j $(nproc) && make install
--- cmake_build.sh
+++
@@ -1,6 +0,0 @@
-#!/bin/bash
-
-BASE=$PWD
-mkdir -p $BASE/build
-
-cd $BASE/build && cmake ../ && make -j $(nproc)
