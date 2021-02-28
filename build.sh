b5b00b9de8b9030d4bd0bda248ff86983c6fdab6
ZhaoMing <zhaoming.274@bytedance.com>
2021-02-28 12:07:34 +0800

    Sync from github

--- build.sh
+++ build.sh
@@ -19,12 +19,12 @@ if test -n "$BUILD_BRANCH"; then
     # this script is run in SCM auto build
     git checkout "$BUILD_BRANCH"
     sudo apt-get update
-    sudo apt-get install libaio-dev -y
+    sudo apt-get install -y libaio-dev
 else
     echo you must ensure libaio-dev have been installed
 fi
 
 git submodule update --init --recursive
 
-cd $BASE/$OUTPUT && cmake ../ -DCMAKE_BUILD_TYPE=Release -DWITH_TESTS=${WITH_TESTS} -DWITH_TOOLS=ON -DWITH_TERARK_ZIP=ON
+cd $BASE/$OUTPUT && cmake ../ -DCMAKE_BUILD_TYPE=RelWithDebInfo -DWITH_TESTS=${WITH_TESTS} -DWITH_TOOLS=ON -DWITH_TERARK_ZIP=ON
 cd $BASE/$OUTPUT && make -j $(nproc) && make install
