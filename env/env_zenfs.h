12875f15471b2cc83b75f08c25a80afb3c70011e
Yuanliang Wang <wangyuanliang@bytedance.com>
2021-03-03 13:31:51 +0800

    zenfs: mv fs_zenfs -> env_zenfs
    Resolve version differences, terarkdb(rocksdb 5.18.3) vs rocksdb 6.8.1
    
    Because zenfs is implemented based on the higher version of rocksdb
    coding, terarkdb is implemented based on the lower version of rocksdb
    coding, this commit is to solve the version difference
    
    Signed-off-by: Yuanliang Wang <wangyuanliang@bytedance.com>
    Reviewed-by: Changlong Chen <chenchanglong@bytedance.com>

--- env/env_zenfs.h
+++ env/env_zenfs.h
@@ -1,5 +1,6 @@
 // Copyright (c) Facebook, Inc. and its affiliates. All Rights Reserved.
 // Copyright (c) 2019-present, Western Digital Corporation
+// Copyright (c) 2020-present, Bytedance Inc.  All rights reserved.
 //  This source code is licensed under both the GPLv2 (found in the
 //  COPYING file in the root directory) and Apache 2.0 License
 //  (found in the LICENSE.Apache file in the root directory).
