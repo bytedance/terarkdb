d81aedbb535093cefab8bbc08fae1997cdc199dd
wangyi <wangyi.ywq@bytedance.com>
2021-03-07 18:46:37 +0800

    add perf

--- db/version_set.cc
+++ db/version_set.cc
@@ -2923,10 +2923,9 @@ void Version::BuildGlobalMap(const ImmutableDBOptions& db_options,
   ROCKS_LOG_INFO(
       info_log_,
       "[BuildGlobalMap] finished build global map, elapsed_nanos=%" PRIu64
-      ", num_entries= %d, data_size=%d, index_size=%d, read_amp=%f perf=%s",
+      ", num_entries= %d, data_size=%d, index_size=%d, read_amp=%f",
       timer.ElapsedNanos(), prop->num_entries, prop->data_size,
-      prop->index_size, prop->read_amp,
-      get_perf_context()->ToString(true).c_str());
+      prop->index_size, prop->read_amp);
   BuildMapIndex(s);
 }
 
