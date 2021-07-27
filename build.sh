4a468ba3630a1f4ac4bdaa620d620c3048ad415a
v1siuol <1098741925@qq.com>
2021-07-26 23:33:02 -0400

    Fix build.sh script (#90)
    * Fix build.sh bug
    * Add debug mode warning if building with test

--- build.sh
+++ build.sh
@@ -1,19 +1,11 @@
 #!/bin/bash
 #
-# WITH_TESTS=1 ./build_cmake.sh
+# WITH_TESTS=1 ./build.sh
 #
 
 BASE=$PWD
 OUTPUT=output
 mkdir -p $OUTPUT
-
-if [ "$WITH_TESTS" == "1" ];then
-  WITH_TESTS=ON
-else
-  WITH_TESTS=OFF
-fi
-
-echo "build $BUILD_TYPE, with_tests = $WITH_TESTS"
 
 if test -n "$BUILD_BRANCH"; then
     # this script is run in SCM auto build
@@ -26,5 +18,16 @@ fi
 
 git submodule update --init --recursive
 
-cd $BASE/$OUTPUT && cmake ../ -DCMAKE_INSTALL_PREFIX=$OUTPUT -DCMAKE_BUILD_TYPE=Release -DWITH_TESTS=${WITH_TESTS} -DWITH_TOOLS=ON -DWITH_TERARK_ZIP=ON
-cd $BASE/$OUTPUT && make -j $(nproc) && make install
+if [ "$WITH_TESTS" == "1" ]; then
+  WITH_TESTS=ON
+  echo "build $BUILD_TYPE, with_tests = $WITH_TESTS"
+  echo "You are building TerarkDB with tests, so debug mode is enabled"
+  cd $BASE/$OUTPUT && cmake ../ -DCMAKE_INSTALL_PREFIX=$OUTPUT -DCMAKE_BUILD_TYPE=Debug -DWITH_TESTS=${WITH_TESTS} -DWITH_TOOLS=ON -DWITH_TERARK_ZIP=ON
+  cd $BASE/$OUTPUT && make -j $(nproc) && make install
+  echo "You are building TerarkDB with tests, so debug mode is enabled"
+else
+  WITH_TESTS=OFF
+  echo "build $BUILD_TYPE, with_tests = $WITH_TESTS"
+  cd $BASE/$OUTPUT && cmake ../ -DCMAKE_INSTALL_PREFIX=$OUTPUT -DCMAKE_BUILD_TYPE=Release -DWITH_TOOLS=ON -DWITH_TERARK_ZIP=ON
+  cd $BASE/$OUTPUT && make -j $(nproc) && make install
+fi
