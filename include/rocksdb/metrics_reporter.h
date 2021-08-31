39da7a48f9267074c95cb0c0d63547c4025064d6
weile.wei <weile.wei@bytedance.com>
2021-08-31 10:25:46 -0500

    add constructor to LatencyHistGuard to support split wal and sst in panel

--- include/rocksdb/metrics_reporter.h
+++ include/rocksdb/metrics_reporter.h
@@ -28,6 +28,33 @@ class HistReporterHandle {
   virtual void AddRecord(size_t val) = 0;
 };
 
+class LatencyHistGuard {
+ public:
+  LatencyHistGuard() = default;
+
+  explicit LatencyHistGuard(HistReporterHandle* handle)
+      : handle_(handle),
+        begin_time_(std::chrono::high_resolution_clock::now()) {}
+
+  ~LatencyHistGuard() {
+    if (handle_ != nullptr) {
+      auto us = std::chrono::duration_cast<std::chrono::microseconds>(
+                    std::chrono::high_resolution_clock::now() - begin_time_)
+                    .count();
+      handle_->AddRecord(us);
+    }
+  }
+
+  void count_now(HistReporterHandle* handle) {
+      handle_ = handle;
+      begin_time_ = std::chrono::high_resolution_clock::now();
+  }
+
+ private:
+  HistReporterHandle* handle_ = nullptr;
+  decltype(std::chrono::high_resolution_clock::now()) begin_time_;
+};
+
 class CountReporterHandle {
  public:
   CountReporterHandle() = default;
