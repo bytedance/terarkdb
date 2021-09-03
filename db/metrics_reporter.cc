5f2fd81cebd059825095d7067d39898f47d0826d
wangyi <wangyi.ywq@bytedance.com>
2021-09-03 18:31:38 +0800

    fix include hpp in header

---
+++ db/metrics_reporter.cc
@@ -0,0 +1,59 @@
+#include "rocksdb/metrics_reporter.h"
+
+#include <inttypes.h>
+
+#include <chrono>
+#include <cstddef>
+#include <string>
+
+#include "util/logging.h"
+
+#define REPORT_DEBUG_STACKTRACE 1
+#if REPORT_DEBUG_STACKTRACE
+#include <boost/stacktrace.hpp>
+#endif
+
+namespace TERARKDB_NAMESPACE {
+
+LatencyHistLoggedGuard::LatencyHistLoggedGuard(HistReporterHandle* handle,
+                                               unsigned int threshold_us = 500 *
+                                                                           1000)
+    : handle_(handle),
+      begin_time_(std::chrono::high_resolution_clock::now()),
+      log_threshold_us_(threshold_us) {
+#if REPORT_DEBUG_STACKTRACE
+  auto stacktrace = new boost::stacktrace::stacktrace();
+  start_stacktrace_ = stacktrace;
+#endif
+}
+
+LatencyHistLoggedGuard::~LatencyHistLoggedGuard() {
+  if (handle_ != nullptr) {
+    auto us = std::chrono::duration_cast<std::chrono::microseconds>(
+                  std::chrono::high_resolution_clock::now() - begin_time_)
+                  .count();
+    handle_->AddRecord(us);
+    if (us > log_threshold_us_) {
+      if (handle_->GetLogger() != nullptr) {
+#if REPORT_DEBUG_STACKTRACE
+        auto stacktrace =
+            static_cast<boost::stacktrace::stacktrace*>(start_stacktrace_);
+        start_stacktrace_ = nullptr;
+        ROCKS_LOG_WARN(
+            handle_->GetLogger(),
+            "[name:%s] [tags:%s]: %" PRIu64 "us\n%s----------\n%s-----------\n",
+            handle_->GetName(), handle_->GetTag(), static_cast<uint64_t>(us),
+            boost::stacktrace::to_string(*stacktrace).c_str(),
+            boost::stacktrace::to_string(boost::stacktrace::stacktrace())
+                .c_str());
+        delete stacktrace;
+#else
+        ROCKS_LOG_WARN(handle_->GetLogger(), "[%s]: %" PRIu64 "us\n",
+                       handle_->GetTag(), static_cast<uint64_t>(us));
+#endif
+      }
+    }
+  }
+}
+
+}  // namespace TERARKDB_NAMESPACE
--- include/rocksdb/metrics_reporter.h
+++ include/rocksdb/metrics_reporter.h
@@ -9,11 +9,6 @@
 #include "rocksdb/env.h"
 #include "rocksdb/terark_namespace.h"
 #include "util/logging.h"
-#define REPORT_DEBUG_STACKTRACE 1
-
-#if REPORT_DEBUG_STACKTRACE
-#include <boost/stacktrace.hpp>
-#endif
 
 namespace TERARKDB_NAMESPACE {
 class HistReporterHandle {
@@ -53,50 +48,14 @@ class LatencyHistGuard {
 class LatencyHistLoggedGuard {
  public:
   explicit LatencyHistLoggedGuard(HistReporterHandle* handle,
-                                  unsigned int threshold = 500 * 1000)
-      : handle_(handle),
-        begin_time_(std::chrono::high_resolution_clock::now()),
-        log_threshold_us_(threshold) {
-#if REPORT_DEBUG_STACKTRACE
-    auto stacktrace = new boost::stacktrace::stacktrace();
-    start_stacktrace_ = stacktrace;
-#endif
-  }
-
-  ~LatencyHistLoggedGuard() {
-    if (handle_ != nullptr) {
-      auto us = std::chrono::duration_cast<std::chrono::microseconds>(
-                    std::chrono::high_resolution_clock::now() - begin_time_)
-                    .count();
-      handle_->AddRecord(us);
-      if (us > log_threshold_us_ && handle_->GetLogger() != nullptr) {
-#if REPORT_DEBUG_STACKTRACE
-        auto stacktrace =
-            static_cast<boost::stacktrace::stacktrace*>(start_stacktrace_);
-        start_stacktrace_ = nullptr;
-        ROCKS_LOG_WARN(
-            handle_->GetLogger(),
-            "[name:%s] [tags:%s]: %" PRIu64 "us\n%s----------\n%s-----------\n",
-            handle_->GetName(), handle_->GetTag(), static_cast<uint64_t>(us),
-            boost::stacktrace::to_string(*stacktrace).c_str(),
-            boost::stacktrace::to_string(boost::stacktrace::stacktrace())
-                .c_str());
-        delete stacktrace;
-#else
-        ROCKS_LOG_WARN(handle_->GetLogger(), "[%s]: %" PRIu64 "us\n",
-                       handle_->GetTag(), static_cast<uint64_t>(us));
-#endif
-      }
-    }
-  }
+                                  unsigned int threshold_us);
+  ~LatencyHistLoggedGuard();
 
  private:
   HistReporterHandle* handle_;
   std::chrono::high_resolution_clock::time_point begin_time_;
   unsigned int log_threshold_us_;
-  Logger* logger_;
-  const char* tag_;
-  void* start_stacktrace_ = nullptr;
+  void* start_stacktrace_;
 };
 
 class CountReporterHandle {
