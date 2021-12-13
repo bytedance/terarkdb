bd6f85fe922a410be78e7343b0bb1cc8e13b7baf
Kuankuan Guo <guokuankuan@bytedance.com>
2021-12-13 18:37:54 +0800

    refactor: refactor metrics related framework to support wdc zenfs, use bzbd/zenfs now

--- build.sh
+++ build.sh
@@ -1,17 +1,30 @@
 #!/bin/bash
 #
-# WITH_TESTS=1 WITH_ZNS=1 ./build.sh
+# WITH_TESTS=1 WITH_ZNS=1 [METRICS_PATH=...] ./build.sh
 #
 
 set -e
 
 BASE=$PWD
 OUTPUT=build
-mkdir -p $OUTPUT
+WITH_BYTEDANCE_METRICS=OFF
 
 if [ -z "$WITH_TESTS" ] || [ -z "${WITH_ZNS}" ]; then
   echo "usage: WITH_TESTS=OFF WITH_ZNS=OFF ./build.sh"
   exit
+fi
+
+if [ -z "$METRICS_PATH" ]; then
+  echo "build without bytedance metrics reporter"
+else
+  WITH_BYTEDANCE_METRICS=ON
+fi
+
+if [ "$WITH_TESTS" == "ON" ]; then
+  BUILD_TYPE=Debug
+  echo "You are building TerarkDB with tests, so debug mode is enabled"
+else
+  BUILD_TYPE=Release
 fi
 
 if test -n "$BUILD_BRANCH"; then
@@ -25,23 +38,18 @@ fi
 
 git submodule update --init --recursive
 
-
-if [ "$WITH_TESTS" == "ON" ]; then
-  BUILD_TYPE=Debug
-  echo "You are building TerarkDB with tests, so debug mode is enabled"
-else
-  BUILD_TYPE=Release
-fi
-
 echo "build = $BUILD_TYPE, with_tests = $WITH_TESTS, with_zns = $WITH_ZNS"
 
+rm -rf $OUTPUT && mkdir -p $OUTPUT
 cd $BASE/$OUTPUT && cmake ../ \
   -DCMAKE_INSTALL_PREFIX=$OUTPUT \
   -DCMAKE_BUILD_TYPE=$BUILD_TYPE \
   -DWITH_TESTS=$WITH_TESTS \
   -DWITH_ZENFS=$WITH_ZNS \
   -DWITH_TOOLS=ON \
-  -DWITH_TERARK_ZIP=ON $@
+  -DWITH_BYTEDANCE_METRICS=$WITH_BYTEDANCE_METRICS \
+  -DBYTEDANCE_METRICS_PATH=$METRICS_PATH \
+  -DWITH_TERARK_ZIP=OFF $@
 
 # compatibility with macOS
 NPROC=$(nproc || sysctl -n hw.logicalcpu)
