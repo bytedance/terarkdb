81d75b7af96fa84716a04215cfd5fe2f2ceb58a8
ZhaoMing <zhaoming.274@bytedance.com>
2022-02-28 16:24:36 +0800

    Code format

--- db/compaction_picker.cc
+++ db/compaction_picker.cc
@@ -1889,8 +1889,8 @@ Compaction* CompactionPicker::PickCompositeCompaction(
       }
       std::shared_ptr<const TableProperties> tp;
       auto s = table_cache_->GetTableProperties(
-          env_options_, *f, &tp,
-          mutable_cf_options.prefix_extractor.get(), true);
+          env_options_, *f, &tp, mutable_cf_options.prefix_extractor.get(),
+          true);
       if (s.IsIncomplete()) {
         if (f->fd.largest_seqno < oldest_snapshot_seqnum) {
           return false;
@@ -2511,8 +2511,7 @@ Compaction* LevelCompactionBuilder::PickLazyCompaction(
                            const DependenceMap& depend_map, Arena* arena,
                            TableReader** table_reader_ptr) {
       return picker->table_cache()->NewIterator(
-          options, picker->env_options(),
-          *file_metadata, depend_map, nullptr,
+          options, picker->env_options(), *file_metadata, depend_map, nullptr,
           mutable_cf_options_.prefix_extractor.get(), table_reader_ptr, nullptr,
           false, arena, true, -1);
     };
@@ -2732,8 +2731,7 @@ Compaction* LevelCompactionBuilder::PickLazyCompaction(
                            const DependenceMap& depend_map, Arena* arena,
                            TableReader** table_reader_ptr) {
       return picker->table_cache()->NewIterator(
-          options, picker->env_options(),
-          *file_metadata, depend_map, nullptr,
+          options, picker->env_options(), *file_metadata, depend_map, nullptr,
           mutable_cf_options_.prefix_extractor.get(), table_reader_ptr, nullptr,
           false, arena, true, -1);
     };
